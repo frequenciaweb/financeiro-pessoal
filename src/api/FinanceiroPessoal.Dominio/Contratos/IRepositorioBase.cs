4595109f96f0c1237f0ceda66c6703859e849184
Paulo Roberto <administrador.paulo@gmail.com>
2023-05-08 21:15:53 -0300

    Criação do Repositorio base

---
+++ src/api/FinanceiroPessoal.Dominio/Contratos/IRepositorioBase.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceiroPessoal.Dominio.Contratos
+{
+    public interface IRepositorioBase<T> where T : class
+    {
+        T Incluir(T obj);
+        void Incluir(List<T> objs);
+        T Alterar(T obj);
+        void Alterar(List<T> objs);
+        void Excluir(T obj);
+        T? Obter(Guid id);
+        IEnumerable<T> Obter();
+    }
+}
---
+++ src/api/FinanceiroPessoal.Infraestrutura/Repositorios/RepositorioBase.cs
@@ -0,0 +1,52 @@
+﻿using FinanceiroPessoal.Dominio.Contratos;
+using FinanceiroPessoal.Infraestrutura.EF;
+
+namespace FinanceiroPessoal.Infraestrutura.Repositorios
+{
+    public abstract class RepositorioBase<T> : IRepositorioBase<T> where T : class
+    {
+        protected readonly FinanceiroPessoalContext Context = default!;
+
+        public RepositorioBase(FinanceiroPessoalContext context)
+        {
+            Context = context;
+        }
+
+        public T Alterar(T obj)
+        {
+            Context.Set<T>().Update(obj);
+            return obj;
+        }
+
+        public void Alterar(List<T> objs)
+        {
+            Context.Set<T>().UpdateRange(objs);
+        }
+
+        public void Excluir(T obj)
+        {
+            Context.Set<T>().Remove(obj);
+        }
+
+        public T Incluir(T obj)
+        {
+            Context.Set<T>().Add(obj);
+            return obj;
+        }
+
+        public void Incluir(List<T> objs)
+        {
+            Context.Set<T>().AddRange(objs);
+        }
+
+        public T? Obter(Guid id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> Obter()
+        {
+            return Context.Set<T>().ToList();
+        }
+    }
+}
